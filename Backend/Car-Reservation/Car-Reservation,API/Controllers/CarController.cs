8e4e4fc8eadee38e7c05ce8e25858415e68c8547
Abdoo <AbdooAwad@gmail.com>
2025-03-17 17:53:07 +0200

    Car Controller

---
+++ Backend/Car-Reservation/Car-Reservation,API/Controllers/CarController.cs
@@ -0,0 +1,25 @@
+﻿using Car_Reservation.APIs.Controllers;
+using Car_Reservation.Repository.Reprositories_Interfaces;
+using Car_Reservation.Repository.UnitOfWork;
+using Car_Reservation_Domain.Entities.CarEntity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Car_Reservation_API.Controllers;
+
+public class CarController(IUnitOfWork unitOfWork) : BaseApiController
+{
+
+    private readonly IUnitOfWork _unitOfWork = unitOfWork;
+    private readonly IGenaricRepository<Car> _carRepo = unitOfWork.Repository<Car>();
+    //Get All Cars
+
+    //Get Car By Id
+    //Get Car By Category
+    //Add Car ---> Admin
+    //Delete Car --> Admin
+    //Update Car Info Car --> Admin
+    //Get All Brand
+
+    // Get All Model For Brand
+}
---
+++ Backend/Car-Reservation/Car-Reservation.Repository/Specfications/CarSpec/CarSpecfications.cs
@@ -0,0 +1,21 @@
+﻿using Car_Reservation.Repository.Specifications;
+using Car_Reservation_Domain.Entities.CarEntity;
+using Microsoft.Data.SqlClient;
+using System.Linq.Expressions;
+namespace Car_Reservation.Repository.Specfications.CarSpec;
+
+public class CarSpecfications: Specification<Car>
+{
+    private static readonly List<Expression<Func<Car, object>>> DefaultIncludes =
+        new() { c=>c.Brand,c=>c.Model };
+    
+    public CarSpecfications(): base(DefaultIncludes) {}
+
+    public CarSpecfications(Expression<Func<Car, bool>>? criteria) : 
+        base(DefaultIncludes,criteria) {}
+    public CarSpecfications(Expression<Func<Car, bool>>? criteria, Expression<Func<Car, object>>? orderBy, SortOrder sortOrder, int? skip = 0, int? take = 10)
+        : base(DefaultIncludes, criteria, orderBy, sortOrder, skip, take)
+    {
+    }
+
+}
